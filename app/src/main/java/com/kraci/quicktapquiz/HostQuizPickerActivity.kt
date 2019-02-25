38310d01d0c517c5f791a1931951abdb94d7a7da
Jakub Kracina <jkracina@sygic.com>
2019-02-25 17:24:06 +0100

    Added main activity (main menu) and activity for pick a quiz

---
+++ app/src/main/java/com/kraci/quicktapquiz/HostQuizPickerActivity.kt
@@ -0,0 +1,12 @@
+package com.kraci.quicktapquiz
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class HostQuizPicker : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_host_quiz_picker)
+    }
+}
--- app/src/main/java/com/kraci/quicktapquiz/QuizViewModel.kt
+++ app/src/main/java/com/kraci/quicktapquiz/QuizViewModel.kt
@@ -3,8 +3,16 @@ package com.kraci.quicktapquiz
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
+import kotlinx.coroutines.experimental.*
+import kotlinx.coroutines.experimental.android.Main
+import kotlin.coroutines.experimental.CoroutineContext
 
 class QuizViewModel(application: Application) : AndroidViewModel(application) {
+
+    private var parentJob = Job()
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+    private val scope = CoroutineScope(coroutineContext)
 
     private val repository: QuizRepository
     val allQuizzes: LiveData<List<Quiz>>
@@ -13,6 +21,15 @@ class QuizViewModel(application: Application) : AndroidViewModel(application) {
         val quizzesDao = QuizDatabase.getDatabase(application).quizDao()
         repository = QuizRepository(quizzesDao)
         allQuizzes = repository.allQuizzes
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
+    }
+
+    fun insert(quiz: Quiz) = scope.launch(Dispatchers.IO) {
+        repository.insert(quiz)
     }
 
 }
