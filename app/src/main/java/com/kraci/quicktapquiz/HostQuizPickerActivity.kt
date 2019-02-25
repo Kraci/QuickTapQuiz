bd6e817e81463a03ca3d2f9dfaab26a12cc2231d
Jakub Kracina <jkracina@sygic.com>
2019-02-25 17:24:57 +0100

    Added activity to name for HostQuizPicker acitivity

--- app/src/main/java/com/kraci/quicktapquiz/HostQuizPickerActivity.kt
+++ app/src/main/java/com/kraci/quicktapquiz/HostQuizPickerActivity.kt
@@ -3,7 +3,7 @@ package com.kraci.quicktapquiz
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class HostQuizPicker : AppCompatActivity() {
+class HostQuizPickerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
